25d57239f7b98a02416f84f16f4fbc8ef6229a51
RyanRothweiler <ryanrothweiler@gmail.com>
2014-04-28 14:20:35 -0700

    no message

--- source/Assets/scripts/Control Stuff/DragControlsPC.js
+++ source/Assets/scripts/Control Stuff/DragControlsPC.js
@@ -329,7 +329,7 @@ function Start ()
 	{
 		//key fading
 		KeyMat.SetColor("_Color", Color(KeyMat.GetColor("_Color").r,KeyMat.GetColor("_Color").g,KeyMat.GetColor("_Color").b, 0));
-		GlowMat.SetColor("_Color", Color(GlowMat.GetColor("_Color").r,GlowMat.GetColor("_Color").g,GlowMat.GetColor("_Color").b, 0));
+//		GlowMat.SetColor("_Color", Color(GlowMat.GetColor("_Color").r,GlowMat.GetColor("_Color").g,GlowMat.GetColor("_Color").b, 0));
 		FadeInKeys();
 	}
 
@@ -1277,7 +1277,7 @@ function FadeOutKeys()
 	do
 	{
 		KeyMat.SetColor("_Color", Color(KeyMat.GetColor("_Color").r, KeyMat.GetColor("_Color").g, KeyMat.GetColor("_Color").b, KeyMat.GetColor("_Color").a - (Time.deltaTime * 2)));
-		GlowMat.SetColor("_Color", Color(GlowMat.GetColor("_Color").r, GlowMat.GetColor("_Color").g, GlowMat.GetColor("_Color").b, GlowMat.GetColor("_Color").a - (Time.deltaTime * 2)));
+//		GlowMat.SetColor("_Color", Color(GlowMat.GetColor("_Color").r, GlowMat.GetColor("_Color").g, GlowMat.GetColor("_Color").b, GlowMat.GetColor("_Color").a - (Time.deltaTime * 2)));
 		yield WaitForSeconds(0.01);
 	} while (KeyMat.GetColor("_Color").a > 0);
 }
@@ -1297,7 +1297,7 @@ function FadeInKeys()
 		KeyMat.SetColor("_Color", Color(KeyMat.GetColor("_Color").r, KeyMat.GetColor("_Color").g, KeyMat.GetColor("_Color").b, KeyMat.GetColor("_Color").a + (Time.deltaTime * 2)));
 //		if (GlowMat.GetColor("_Color").a < )
 //		{
-			GlowMat.SetColor("_Color", Color(GlowMat.GetColor("_Color").r, GlowMat.GetColor("_Color").g, GlowMat.GetColor("_Color").b, GlowMat.GetColor("_Color").a + (Time.deltaTime * 2)));
+//			GlowMat.SetColor("_Color", Color(GlowMat.GetColor("_Color").r, GlowMat.GetColor("_Color").g, GlowMat.GetColor("_Color").b, GlowMat.GetColor("_Color").a + (Time.deltaTime * 2)));
 //		}
 		yield WaitForSeconds(0.01);
 	} while (KeyMat.GetColor("_Color").a < 1);
--- source/Assets/scripts/Level Select Stuff/KeyHolderController.js
+++ source/Assets/scripts/Level Select Stuff/KeyHolderController.js
@@ -6,19 +6,31 @@
 private var target : Vector3;
 private var velocity : Vector3;
 private var offset : Vector3;
+private var endSet = false;
 
 function Start () 
 {
 	//set offset
 	offset = Camera.main.transform.position - transform.position;
+	offset.x -= 20;
 }
 
 function Update () 
 {
-	//update target
-	target = Camera.main.transform.position - offset;
-	target.y = transform.position.y;
-	target.z = transform.position.z;
+	//move to target
+	if (!Camera.main.GetComponent(DragControlsPC).introing)
+	{
+		//update target
+		target = Camera.main.transform.position - offset;
+		target.y = transform.position.y;
+		target.z = transform.position.z;
+		
+		transform.position = Vector3.SmoothDamp(transform.position, target, velocity, 0.15, 50, Time.deltaTime);
+	}
 	
-	transform.position = Vector3.SmoothDamp(transform.position, target, velocity, 0.15, 50, Time.deltaTime);
+	if (!endSet && Camera.main.GetComponent(DragControlsPC).toLevel)
+	{
+		endSet = true;
+		offset.x += 20;
+	}
 }
