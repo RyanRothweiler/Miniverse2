eb46d32231eb92446793c1df67f03a72a44db40b
RyanRothweiler <ryanrothweiler@gmail.com>
2014-04-28 10:16:35 -0700

    no message

--- source/Assets/scripts/Background Stuff/NebulaBackground.js
+++ source/Assets/scripts/Background Stuff/NebulaBackground.js
@@ -11,7 +11,7 @@ function Start ()
 	//get drag controls script
 	DragControls = Camera.main.GetComponent(DragControlsPC);
 	
-	AnimSpeed = Random.Range(0.3, 0.5);
+	AnimSpeed = Random.Range(0.1, 0.3);
 	
 	//set animation frame and speed
 	animation["Default Take"].speed = AnimSpeed;
